108e8fdb214d38f957b993b2ddb49b3f4a4f9f11
Sickyman678 <97682657+Sickyman678@users.noreply.github.com>
2022-03-04 12:42:48 +0000

    Add files via upload

---
+++ guacamoleb/device.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+$(call inherit-product, device/oneplus/sm8150-common/common.mk)
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/oneplus/guacamoleb/guacamoleb-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-xtended
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_pahu.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_pahu.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml
+
+# Device init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom
+
+USE_GAPPS := true
+
+# OPFeature
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackagesDevice
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Wifi Overlay
+PRODUCT_PACKAGES += \
+    OnePlus7WifiOverlay
---
+++ guacamoleb/xtended_guacamoleb.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from guacamoleb device
+$(call inherit-product, device/oneplus/guacamoleb/device.mk)
+
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := xtended_guacamoleb
+PRODUCT_DEVICE := guacamoleb
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := GM1905
+PRODUCT_MANUFACTURER := OnePlus
+
+PRODUCT_AAPT_CONFIG := xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE=OnePlus7 \
+    PRODUCT_NAME=OnePlus7
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+XTENDED_BUILD_MAINTAINER := sickyman678
